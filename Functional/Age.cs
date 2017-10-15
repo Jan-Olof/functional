2c3ee8d7ac0851f18b9b082c10b7e91f7f39f2d9
Jan-Olof Nilsson <janolof@gmail.com>
2017-10-15 20:12:16 +0200

    Added Age.

---
+++ Functional/Age.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Functional
+{
+    public class Age
+    {
+        public Age(int value)
+        {
+            if (!IsValid(value))
+            {
+                throw new ArgumentException($"{value} is not a valid age.");
+            }
+
+            Value = value;
+        }
+
+        private int Value { get; }
+
+        public static bool operator <(Age l, Age r)
+            => l.Value < r.Value;
+
+        public static bool operator <(Age l, int r)
+            => l < new Age(r);
+
+        public static bool operator >(Age l, Age r)
+            => l.Value > r.Value;
+
+        public static bool operator >(Age l, int r)
+            => l > new Age(r);
+
+        private static bool IsValid(int age)
+            => 0 <= age && age < 120;
+    }
+}
