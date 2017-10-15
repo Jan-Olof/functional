231712d1c5118cb29f16e7d73d4c52159dcff486
Jan-Olof Nilsson <janolof@gmail.com>
2017-10-15 17:00:29 +0200

    One more functional test.

---
+++ FunctionalTests/DateNotPastValidatorTests.cs
@@ -0,0 +1,29 @@
+﻿using Functional.Validators;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace FunctionalTests
+{
+    [TestClass]
+    public class DateNotPastValidatorTests
+    {
+        private readonly DateTime _presentDate = new DateTime(2017, 10, 15);
+
+        [DataRow(+1, true)]
+        [DataRow(0, true)]
+        [DataRow(-1, false)]
+        [DataTestMethod]
+        public void WhenTransferDateIsPast_ThenValidatorFails(int offset, bool expected)
+        {
+            // Given
+            var sut = new DateNotPastValidator(_presentDate);
+            var cmd = new MakeTransfer { Date = _presentDate.AddDays(offset) };
+
+            // When
+            var result = sut.IsValid(cmd);
+
+            // Then
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
