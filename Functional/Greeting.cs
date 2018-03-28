a3c48e7235fca4362a06f20247f4f2ca8ccda570
Jan-Olof Nilsson <janolof@gmail.com>
2018-03-28 20:47:28 +0200

    Map option.

---
+++ Functional/Greeting.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Functional
+{
+    public class Greeting
+    {
+        public readonly Func<string, string> Greet = name => $"hello, {name}";
+    }
+}
---
+++ FunctionalTests/GreetingTests.cs
@@ -0,0 +1,39 @@
+﻿using Functional;
+using LaYumba.Functional;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static LaYumba.Functional.F;
+
+namespace FunctionalTests
+{
+    [TestClass]
+    public class GreetingTests
+    {
+        [TestMethod]
+        public void WhenGreeting_ThenGreetJohn()
+        {
+            // Given
+            var sut = new Greeting();
+            Option<string> john = Some("John");
+
+            // When
+            var result = john.Map(sut.Greet);
+
+            // Then
+            Assert.AreEqual(Some("hello, John"), result);
+        }
+
+        [TestMethod]
+        public void WhenGreeting_ThenGreetNone()
+        {
+            // Given
+            var sut = new Greeting();
+            Option<string> nobody = None;
+
+            // When
+            var result = nobody.Map(sut.Greet);
+
+            // Then
+            Assert.AreEqual(None, result);
+        }
+    }
+}
