e6e3934a91c8fcc62873c4f35715ebd0bd8ade2f
Jan-Olof Nilsson <janolof@gmail.com>
2018-04-11 14:04:35 +0200

    Added BindTasks.

---
+++ Functional/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Functional.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static async Task<IEnumerable<T2>> BindTasks<T1, T2>(this IEnumerable<T1> collection, Func<T1, Task<IEnumerable<T2>>> func)
+        {
+            var tasks = collection.Select(func);
+            var nestedresults = await Task.WhenAll(tasks);
+            return nestedresults.SelectMany(n => n);
+        }
+    }
+}
